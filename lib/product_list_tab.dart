862a949281c1960e59eb226fb40d1b40569b8bb8
ernestrlee <ernestrlee@yahoo.com>
2020-06-26 11:23:29 -0700

    Added slivers to display products.

--- lib/product_list_tab.dart
+++ lib/product_list_tab.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-
+import 'package:cupertino_store/product_row_item.dart';
 import 'model/app_state_model.dart';
 
 class ProductListTab extends StatelessWidget {
@@ -8,11 +8,31 @@ class ProductListTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
-        return const CustomScrollView(
+        final products = model.getProducts();
+        return CustomScrollView(
+          semanticChildCount: products.length,
           slivers: <Widget>[
             CupertinoSliverNavigationBar(
               largeTitle: Text('Cupertino Store'),
             ),
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 8),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+                    if (index < products.length) {
+                      return ProductRowItem(
+                        index: index,
+                        product: products[index],
+                        lastItem: index == products.length - 1,
+                      );
+                    }
+
+                    return null;
+                  },
+                ),
+              ),
+            )
           ],
         );
       },
